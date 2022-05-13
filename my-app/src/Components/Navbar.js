c53346eb6707d4e68273989f3da9187294e7b559
yasirah-tech <yasirahboyce1@gmail.com>
2022-05-13 11:55:19 -0400

    added Nav Links that goes to our impacts and homepage. additional styling will be done once I complete the task of making a Nav folder to hold the Nav component and its corresponding css file

--- my-app/src/Components/Navbar.js
+++ my-app/src/Components/Navbar.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import { Nav, NavItem, NavLink } from "reactstrap";
 
-function Navbar(){
-    return(
-        <h1>Hello</h1>
-    )
+function Navbar() {
+  return (
+    <Nav>
+      <NavItem>
+        <Link to={"/"}>
+          {" "}
+          <NavLink active href="">
+            Home
+          </NavLink>
+        </Link>
+      </NavItem>
+      <NavItem>
+        <Link to={"/impacts"}>
+          <NavLink>Impacts</NavLink>
+        </Link>
+      </NavItem>
+      <NavItem>
+        <Link to={"/SignIn"}>
+          <NavLink>
+            Log Out
+          </NavLink>
+        </Link>
+      </NavItem>
+    </Nav>
+  );
 }
 
-export default Navbar
+export default Navbar;

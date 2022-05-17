04d2aadbc3278e7ad3684b8aad99f23aeb59595c
AndrewKins <andrewm.enoe@gmail.com>
2022-05-17 16:09:17 -0400

    changed map code

--- my-app/src/Components/Homepage/DoulaMap/Map.js
+++ my-app/src/Components/Homepage/DoulaMap/Map.js
@@ -1,46 +1,45 @@
-import React from 'react'
-import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+import React from 'react';
+import { GoogleMap, useJsApiLoader, MarkerF, useLoadScript } from '@react-google-maps/api'
 
-const containerStyle = {
-  width: '400px',
-  height: '400px',
-};
-
-const center = {
-  lat: 	40.730610,
-  lng: -73.935242
-};
-
-function DoulaMapLoader() {
-  const { isLoaded } = useJsApiLoader({
-    id: 'google-map-script',
-    googleMapsApiKey: "AIzaSyCzaZkyjl3bdTuYl9pHS1lsVvE5oLFGZKE"
-  })
-
-  const [map, setMap] = React.useState(null)
-
-  const onLoad = React.useCallback(function callback(map) {
-    const bounds = new window.google.maps.LatLngBounds(center);
-    map.fitBounds(bounds);
-    setMap(map)
-  }, [])
-
-  const onUnmount = React.useCallback(function callback(map) {
-    setMap(null)
-  }, [])
-
-  return isLoaded ? (
-      <GoogleMap
-        mapContainerStyle={containerStyle}
-        center={center}
-        zoom={8}
-        onLoad={onLoad}
-        onUnmount={onUnmount}
-      >
-        { /* Child components, such as markers, info windows, etc. */ }
-        <></>
-      </GoogleMap>
-  ) : <></>
+function Spinner() {
+  return 'Loading...'
 }
 
-export default React.memo(DoulaMapLoader)
+const API_KEY = "AIzaSyCzaZkyjl3bdTuYl9pHS1lsVvE5oLFGZKE"
+
+function App() {
+  const { isLoaded, loadError } = useJsApiLoader({
+    googleMapsApiKey: API_KEY
+  })
+
+  if (loadError) {
+    return <div>Map cannot be loaded right now, sorry.</div>
+  }
+
+  return isLoaded ? (
+    <GoogleMap
+      mapContainerStyle={{
+         width: '700px',
+        height: '700px'
+      }}
+      zoom={10}
+      center={{
+        lat: 	40.730610,
+        lng: -73.935242
+      }}
+    >
+      <MarkerF position={{
+        lat: 40.668470,
+        lng: -73.982530
+      }}
+      />
+      <MarkerF position={{
+      lat: 40.683970,
+    lng: -73.931860
+      }}
+      />
+    </GoogleMap>
+  ): <Spinner />
+}
+
+export default App;

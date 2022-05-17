0cab7a0631439e3dc2fe50262fe94823141e36fb
Matt-Tan15 <tanmatt804@gmail.com>
2022-05-17 15:44:30 -0400

    fetched posts, added timestamps and luxon dependency

--- my-app/src/Components/Home/post.js
+++ my-app/src/Components/Home/post.js
@@ -1,7 +1,9 @@
 import CommentForm from "./commentForm";
 import Comments from "./comments";
+import { DateTime } from "luxon";
 
 function Post(props) {
+  const post = props.p;
   return (
     <li className="timeline-item">
       <div className="card card-white grid-margin">
@@ -12,25 +14,19 @@ function Post(props) {
               alt=""
             />
             <p>
-              Vikash smith <span>posted a status</span>
+              {post.first_name} {post.last_name}
             </p>
-            <small>3 hours ago</small>
+            <small>{DateTime.fromISO(post.created_at).toRelative()}</small>
           </div>
           <div className="timeline-item-post">
             <p>
-              Elavita veritatis et quasi architecto beatae vitae dicta sunt
-              explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur
-              aut odit aut fugit, sed quia consequuntur.
+              {post.content}
             </p>
             <div className="timeline-options">
-              <a href="#">
-                <i className="fa fa-thumbs-up"></i> Like (15)
-              </a>
               <a href="#">
                 <i className="fa fa-comment"></i> Comment (4)
               </a>
             </div>
-            <Comments />
             <Comments />
             <CommentForm />
           </div>

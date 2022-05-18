9aed6fa35a1c8d69d259b31cff1abd24379fe860
Matt-Tan15 <tanmatt804@gmail.com>
2022-05-18 16:25:07 -0400

    can update posts now

--- my-app/src/Components/Home/dropdown.js
+++ my-app/src/Components/Home/dropdown.js
@@ -2,7 +2,7 @@ import AppContext from "../../context/AppContext";
 import { useContext } from "react";
 
 function Dropdown(props) {
-  const { posts, setPosts } = useContext(AppContext);
+  const { posts, setPosts, currInput, setcurrInput } = useContext(AppContext);
 
   function handleDelete() {
     const postId = props.postid;
@@ -16,8 +16,28 @@ function Dropdown(props) {
     }
 
     deleteAPost(postId);
-    const newPostFeed = posts.filter(post => post.id !== +postId);
+    const newPostFeed = posts.filter((post) => post.id !== +postId);
     setPosts([...newPostFeed]);
+  }
+
+  function handleUpdatePost() {
+    const postId = props.postid;
+    async function updateAPost(id) {
+      const response = await fetch(`http://localhost:8000/post/${id}`, {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          content: currInput
+        })
+      });
+      const data = await response.json();
+      const updatedContent = data.content;
+      const foundPost = posts.findIndex(post => post.id === id);
+      posts[foundPost].content = updatedContent;
+      setPosts([...posts]);
+    }
+    
+    updateAPost(postId);
   }
 
   return (
@@ -33,16 +53,71 @@ function Dropdown(props) {
       </button>
       <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
         <li>
-          <option className="dropdown-item" href="#">
+          <button
+            type="button"
+            class="btn"
+            data-bs-toggle="modal"
+            data-bs-target="#exampleModal"
+          >
             Edit
-          </option>
+          </button>
         </li>
         <li>
-          <option onClick={handleDelete} className="dropdown-item" href="#">
+          <button onClick={handleDelete} className="dropdown-item" href="#">
             Delete
-          </option>
+          </button>
         </li>
       </ul>
+
+      <div
+        class="modal fade"
+        id="exampleModal"
+        tabindex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">
+                Modal title
+              </h5>
+              <button
+                type="button"
+                class="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div class="modal-body">
+              <textarea
+                className="form-control"
+                placeholder="What's on your mind?"
+                rows="4"
+                value={currInput}
+                onChange={(e) => setcurrInput(e.target.value)}
+              ></textarea>
+            </div>
+            <div class="modal-footer">
+              <button
+                type="button"
+                class="btn btn-secondary"
+                data-bs-dismiss="modal"
+              >
+                Close
+              </button>
+              <button
+                type="button"
+                class="btn btn-primary"
+                data-bs-dismiss="modal"
+                onClick={handleUpdatePost}
+              >
+                Save changes
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }

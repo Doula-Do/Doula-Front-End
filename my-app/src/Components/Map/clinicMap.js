b422e6d3d0ba1492ee3ba6642c60c41bb0b4ec16
AndrewKins <andrewm.enoe@gmail.com>
2022-05-26 12:44:09 -0400

    updated map page

--- my-app/src/Components/Map/clinicMap.js
+++ my-app/src/Components/Map/clinicMap.js
@@ -1,3 +1,4 @@
+import './clinicMap.css'
 import React from 'react'
 import Navbar from '../navbar/Navbar.js';
 import Footer from '../footer/footer.js';
@@ -6,18 +7,19 @@ import DoulaMapLoader from './DoulaMap/Map.js';
 // import familyimg from '../images/familyimg.jpeg'
 function Homepage() {
     return(
-        <div>
+        <div className='background'>
         <Navbar/>
-        <h4>DoulaDo is a community-based application that allows expecting parent(s) to connect with caring, supportive, and experience Doula's in their neighborhood.</h4>
-        <h4>Approach birth as an opportunity to have an adventureon the way to meeting your baby (rather than ordeal to be endured).
+        <div className='pagebody' >
+        <h4 className='Main'>DoulaDo is a community-based application that allows expecting parent(s) to connect with caring, supportive, and experience Doula's in their neighborhood.</h4>
+        <h4 className='Main'>Approach birth as an opportunity to have an adventureon the way to meeting your baby (rather than ordeal to be endured).
 -Anna Wilde</h4>
-        <form className='zipCodeForm'>
-            <label>Enter your Zip Code and Find a Doula Near You!
-                <br></br>
-            <input type="text" name="name" />
-            </label>
-        </form>
-        <DoulaMapLoader/>
+<h2>Find a Doula Near You!</h2>
+        </div>
+        
+        
+        <div className= 'Map'>
+        <DoulaMapLoader className= 'Map'/>
+        </div>
         <Footer/>
         </div>
     )

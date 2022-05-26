5ef6519b55b1aa52eab713d6dd88152e6a7c66f5
Matt-Tan15 <tanmatt804@gmail.com>
2022-05-26 12:09:00 -0400

    carousel works now

--- my-app/src/Components/Home/doulasConnected.js
+++ my-app/src/Components/Home/doulasConnected.js
@@ -5,28 +5,36 @@ import AppContext from "../../context/AppContext";
 function ConnectedDoulas() {
   const {user} = useContext(AppContext);
   const [users, setUsers] = useState([]);
+  const [doulas, setDoulas] = useState([]);
   useEffect(() => {
     fetch('http://localhost:8000/users')
     .then(response => response.json())
     .then(data => setUsers(data.data.filter(people => people.id !== +user.id)));
+
+    fetch("http://localhost:8000/users")
+      .then((response) => response.json())
+      .then((data) =>
+        setDoulas(data.data.filter((people) => people.is_doula === true))
+      );
   }, [])
 
   return (
     <div className="card card-white grid-margin text-center">
       <div className="card-heading clearfix">
-        <h4 className="card-title">Doulas Connected</h4>
-      </div>
-      <div className="card-body">
-        <p>
-        </p>
-      </div>
-      <hr />
-      <div className="card-heading clearfix">
-        <h4 className="card-title">Recommended Doulas</h4>
+        <h4 className="card-title">Doulas</h4>
       </div>
       <div className="card-body">
         <ul>
-          {users.map(user => <li><Link to={`/user/${user.id}`} className="recommendedNames">{user.first_name} {user.last_name}</Link></li>)}
+          {doulas.map(doula => <li><Link to={`/user/${doula.id}`}><button className="btn w-50 mx-auto fw-bold modalButton">{doula.first_name} {doula.last_name}</button></Link></li>)}
+        </ul>
+      </div>
+      <hr />
+      <div className="card-heading clearfix">
+        <h4 className="card-title">Users</h4>
+      </div>
+      <div className="card-body">
+        <ul>
+          {users.map(user => <li><Link to={`/user/${user.id}`} className="recommendedNames"><button className="btn w-50 mx-auto fw-bold modalButton">{user.first_name} {user.last_name}</button></Link></li>)}
         </ul>
       </div>
     </div>
--- my-app/src/Components/ImpactsPage/Impacts.js
+++ my-app/src/Components/ImpactsPage/Impacts.js
@@ -1,82 +1,138 @@
-import Footer from "../footer/footer";
 import Navbar from "../navbar/Navbar";
+import Menu from "../Home/menu";
+import ConnectedDoulas from "../Home/doulasConnected";
 import "./impacts.css";
-import { useEffect } from "react";
+import { Navigate } from "react-router-dom";
+import AppContext from "../../context/AppContext";
+import { useContext } from "react";
 
 function Impacts() {
-  return (
+  const { isAuth } = useContext(AppContext);
+
+  return !isAuth ? (
+    <Navigate to="/" />
+  ) : (
     <div>
       <Navbar />
-      <div className="testimonials-clean contentBody">
-        <div className="container">
-          <div className="intro">
-            <h2 className="text-center">Testimonials </h2>
-            <p className="text-center">
-              Doulas have made such a huge impact with families! Read what they
-              have to say below.
-            </p>
-          </div>
-          <div className="carousel slide demo" data-ride="carousel">
-            <div className="carousel-inner">
-              <div className="carousel-item active">
-                <div className="carousel-caption">
-                  <p>
-                    “You know how they say it takes a village to raise a baby?
-                    Well, one good doula is equal to that whole village!”
-                  </p>
-                  <img src="https://i.imgur.com/lE89Aey.jpg" />
-                  <div className="image-caption">— Emily D.</div>
-                  <p className="title">CEO of Company Inc.</p>
+      <div className="page-inner no-page-title pageContainer">
+        <div id="main-wrapper">
+          <div className="row">
+            <div className="col-lg-5 col-xl-3">
+              <div className="card card-white grid-margin">
+                <div className="card-heading clearfix">
+                  <h4 className="card-title text-center">Menu</h4>
                 </div>
+                <Menu />
               </div>
-              <div className="carousel-item">
-                <div className="carousel-caption">
-                  <p>
-                    If Shai Reznik's TDD videos don't convince you to add
-                    automated testing your code, I don't know what will.This was
-                    the very best explanation of frameworks for brginners that
-                    I've ever seen.
+            </div>
+            <div className="col-lg-7 col-xl-6">
+              <div
+                className="container p-2"
+                style={{ backgroundColor: "white" }}
+              >
+                <div className="intro">
+                  <h2 className="text-center">Testimonials </h2>
+                  <p className="text-center p-3">
+                    Doulas have made such a huge impact with families! Read what
+                    they have to say below.
                   </p>
-                  <img
-                    src="https://i.imgur.com/QptVdsp.jpg"
-                    className="img-fluid"
-                  />
-                  <div className="image-caption">Cromption Greves</div>
                 </div>
-              </div>
-              <div className="carousel-item">
-                <div className="carousel-caption">
-                  <p>
-                    If Shai Reznik's TDD videos don't convince you to add
-                    automated testing your code, I don't know what will.This was
-                    the very best explanation of frameworks for brginners that
-                    I've ever seen.
-                  </p>
-                  <img
-                    src="https://i.imgur.com/jQWThIn.jpg"
-                    className="img-fluid"
-                  />
-                  <div className="image-caption">Harry Mon</div>
+                <div
+                  id="carouselExampleCaptions"
+                  class="carousel slide"
+                  data-bs-ride="carousel"
+                >
+                  <div class="carousel-indicators">
+                    <button
+                      type="button"
+                      data-bs-target="#carouselExampleCaptions"
+                      data-bs-slide-to="0"
+                      class="active"
+                      aria-current="true"
+                      aria-label="Slide 1"
+                    ></button>
+                    <button
+                      type="button"
+                      data-bs-target="#carouselExampleCaptions"
+                      data-bs-slide-to="1"
+                      aria-label="Slide 2"
+                    ></button>
+                    <button
+                      type="button"
+                      data-bs-target="#carouselExampleCaptions"
+                      data-bs-slide-to="2"
+                      aria-label="Slide 3"
+                    ></button>
+                  </div>
+                  <div class="carousel-inner">
+                    <div class="carousel-item active">
+                      <div class="carousel-caption d-none d-md-block">
+                        <p>
+                          “You know how they say it takes a village to raise a
+                          baby? Well, one good doula is equal to that whole
+                          village!”
+                        </p>
+                        <img src="https://media.istockphoto.com/vectors/default-profile-picture-avatar-photo-placeholder-vector-illustration-vector-id1223671392?k=20&m=1223671392&s=612x612&w=0&h=lGpj2vWAI3WUT1JeJWm1PRoHT3V15_1pdcTn2szdwQ0=" />
+                        <div className="image-caption">— Emily D.</div>
+                        <p className="title">CEO of Company Inc.</p>
+                      </div>
+                    </div>
+                    <div class="carousel-item">
+                      <div class="carousel-caption d-none d-md-block">
+                        <p>
+                          “You know how they say it takes a village to raise a
+                          baby? Well, one good doula is equal to that whole
+                          village!”
+                        </p>
+                        <img src="https://media.istockphoto.com/vectors/default-profile-picture-avatar-photo-placeholder-vector-illustration-vector-id1223671392?k=20&m=1223671392&s=612x612&w=0&h=lGpj2vWAI3WUT1JeJWm1PRoHT3V15_1pdcTn2szdwQ0=" />
+                        <div className="image-caption">— Emily D.</div>
+                        <p className="title">CEO of Company Inc.</p>
+                      </div>
+                    </div>
+                    <div class="carousel-item">
+                      <div class="carousel-caption d-none d-md-block">
+                        <p>
+                          “You know how they say it takes a village to raise a
+                          baby? Well, one good doula is equal to that whole
+                          village!”
+                        </p>
+                        <img src="https://media.istockphoto.com/vectors/default-profile-picture-avatar-photo-placeholder-vector-illustration-vector-id1223671392?k=20&m=1223671392&s=612x612&w=0&h=lGpj2vWAI3WUT1JeJWm1PRoHT3V15_1pdcTn2szdwQ0=" />
+                        <div className="image-caption">— Emily D.</div>
+                        <p className="title">CEO of Company Inc.</p>
+                      </div>
+                    </div>
+                  </div>
+                  <button
+                    class="carousel-control-prev"
+                    type="button"
+                    data-bs-target="#carouselExampleCaptions"
+                    data-bs-slide="prev"
+                  >
+                    <span
+                      class="carousel-control-prev-icon Arrow"
+                      aria-hidden="true"
+                    ></span>
+                    <span class="visually-hidden">Previous</span>
+                  </button>
+                  <button
+                    class="carousel-control-next"
+                    type="button"
+                    data-bs-target="#carouselExampleCaptions"
+                    data-bs-slide="next"
+                  >
+                    <span
+                      class="carousel-control-next-icon Arrow"
+                      aria-hidden="true"
+                    ></span>
+                    <span class="visually-hidden">Next</span>
+                  </button>
                 </div>
               </div>
             </div>
-            <a className="carousel-control-prev" data-slide="prev">
-              <i className="fas fa-arrow-left"></i>
-            </a>
-            <a className="carousel-control-next" data-slide="next">
-              <i className="fas fa-arrow-right"></i>
-            </a>
+            <div className="col-lg-12 col-xl-3">
+              <ConnectedDoulas />
+            </div>
           </div>
-          {/* <div className="row people">
-            <Testimonial />
-            <Testimonial />
-            <Testimonial />
-          </div>
-          <div className="row people">
-            <Testimonial />
-            <Testimonial />
-            <Testimonial />
-          </div> */}
         </div>
       </div>
     </div>

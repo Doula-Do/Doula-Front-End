42d1f877ca1392337501f95aa6a174108cbd418c
Matt-Tan15 <tanmatt804@gmail.com>
2022-05-26 16:14:44 -0400

    can update profile now

--- my-app/src/Components/profiles/userProfile.js
+++ my-app/src/Components/profiles/userProfile.js
@@ -7,16 +7,23 @@ import { useContext } from "react";
 import ConnectedDoulas from "../Home/doulasConnected";
 import { useParams, Navigate } from "react-router-dom";
 import { useEffect, useState } from "react";
+import EditIcon from "@mui/icons-material/Edit";
 import "./profile.css";
 
 function UserProfile() {
   const { user, isAuth } = useContext(AppContext);
   const [profileUser, setprofileUser] = useState({});
   const [profilePosts, setprofilePosts] = useState([]);
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [birthday, setBirthday] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [medicaid, setMedicaid] = useState("No");
+  const [doulaSkillSet, setDoulaSkillSet] = useState("");
   const { id } = useParams();
-  
+
   useEffect(() => {
-    if (!user.id) return
+    if (!user.id) return;
     fetch(`http://localhost:8000/user/${id}`)
       .then((response) => response.json())
       .then((data) => setprofileUser({ ...data[0] }));
@@ -28,7 +35,46 @@ function UserProfile() {
       );
   }, [id]);
 
-  return !isAuth ? <Navigate to="/" /> : (
+  function handleEdit() {
+    const currId = id;
+
+    const newUserInfo = {
+      first_name: firstName,
+      last_name: lastName,
+      birthday,
+      phone_number: phoneNumber,
+      medicaid,
+      email : profileUser.email,
+      doula_skillset: doulaSkillSet,
+    };
+
+    if (newUserInfo.first_name === "") newUserInfo.first_name = profileUser.first_name;
+    if (newUserInfo.last_name === "") newUserInfo.last_name = profileUser.last_name;
+    if (newUserInfo.medicaid === "Yes") {
+      newUserInfo.medicaid = true;
+    } else newUserInfo.medicaid = false;
+
+    async function editProfile() {
+      const response = await fetch(`http://localhost:8000/user/${currId}`, {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(newUserInfo),
+      });
+    }
+
+    editProfile();
+    setprofileUser({...profileUser, ...newUserInfo});
+    setFirstName("");
+    setLastName("");
+    setBirthday("");
+    setPhoneNumber("");
+    setMedicaid("");
+    setDoulaSkillSet("");
+  }
+
+  return !isAuth ? (
+    <Navigate to="/" />
+  ) : (
     <div>
       <Navbar />
       <div className="page-inner no-page-title pageContainer">
@@ -55,6 +101,104 @@ function UserProfile() {
                       <h2>User Profile</h2>
                     </>
                   )}
+                  {+profileUser.id === +user.id && (
+                    <>
+                      <EditIcon
+                        className="edit"
+                        type="button"
+                        data-bs-toggle="modal"
+                        data-bs-target="#exampleModal"
+                      />
+                    </>
+                  )}
+                </div>
+                <div
+                  class="modal fade"
+                  id="exampleModal"
+                  tabindex="-1"
+                  aria-labelledby="exampleModalLabel"
+                  aria-hidden="true"
+                >
+                  <div class="modal-dialog">
+                    <div class="modal-content">
+                      <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">
+                          Edit Post
+                        </h5>
+                        <button
+                          type="button"
+                          class="btn-close"
+                          data-bs-dismiss="modal"
+                          aria-label="Close"
+                        ></button>
+                      </div>
+                      <div class="modal-body">
+                        First Name
+                        <textarea
+                          className="form-control"
+                          rows="1"
+                          value={firstName}
+                          onChange={(e) => setFirstName(e.target.value)}
+                        ></textarea>
+                        Last Name
+                        <textarea
+                          className="form-control"
+                          rows="1"
+                          value={lastName}
+                          onChange={(e) => setLastName(e.target.value)}
+                        ></textarea>
+                        Birthday
+                        <textarea
+                          className="form-control"
+                          rows="1"
+                          value={birthday}
+                          onChange={(e) => setBirthday(e.target.value)}
+                        ></textarea>
+                        Phone Number
+                        <textarea
+                          className="form-control"
+                          rows="1"
+                          value={phoneNumber}
+                          onChange={(e) => setPhoneNumber(e.target.value)}
+                        ></textarea>
+                        Medicaid
+                        <textarea
+                          className="form-control"
+                          rows="1"
+                          value={medicaid}
+                          onChange={(e) => setMedicaid(e.target.value)}
+                        ></textarea>
+                        {profileUser.is_doula === true && (
+                          <>
+                            Doula Skillset
+                            <textarea
+                              className="form-control"
+                              rows="1"
+                              value={doulaSkillSet}
+                              onChange={(e) => setDoulaSkillSet(e.target.value)}
+                            ></textarea>
+                          </>
+                        )}
+                      </div>
+                      <div class="modal-footer">
+                        <button
+                          type="button"
+                          class="btn btn-secondary modalButton"
+                          data-bs-dismiss="modal"
+                        >
+                          Close
+                        </button>
+                        <button
+                          type="button"
+                          class="btn btn-primary modalButton"
+                          data-bs-dismiss="modal"
+                          onClick={handleEdit}
+                        >
+                          Save changes
+                        </button>
+                      </div>
+                    </div>
+                  </div>
                 </div>
                 <div className="panel-body bio-graph-info">
                   <div className="row">
@@ -70,17 +214,23 @@ function UserProfile() {
                     </div>
                     <div className="bio-row">
                       <p>
-                        <span>Birthday </span>: {profileUser.birthday}
+                        <span>Birthday </span>: {!profileUser.birthday && (<>N/A</>)}{" "}{profileUser.birthday}
                       </p>
                     </div>
                     <div className="bio-row">
                       <p>
-                        <span>Number</span>: {!profileUser.phone_number && (<>N/A</>)} {profileUser.phone_number > 0 && (<>{profileUser.phone_number}</>)}
+                        <span>Number</span>:{" "}
+                        {!profileUser.phone_number && <>N/A</>}{" "}
+                        {profileUser.phone_number > 0 && (
+                          <>{profileUser.phone_number}</>
+                        )}
                       </p>
                     </div>
                     <div className="bio-row">
                       <p>
-                        <span>Medicaid </span>: {profileUser.medicaid === true && (<>Yes</>)} {profileUser.medicaid === false && (<>No</>)}
+                        <span>Medicaid </span>:{" "}
+                        {profileUser.medicaid === true && <>Yes</>}{" "}
+                        {profileUser.medicaid === false && <>No</>}
                       </p>
                     </div>
                     <div className="bio-row">
@@ -97,7 +247,8 @@ function UserProfile() {
                       <>
                         <div className="bio-row">
                           <p>
-                            <span>Doula Skillset </span>: {profileUser.doula_skillset}
+                            <span>Doula Skillset </span>:{" "}
+                            {!profileUser.doula_skillset && (<>N/A</>)}{" "}{profileUser.doula_skillset}
                           </p>
                         </div>
                       </>
@@ -108,12 +259,7 @@ function UserProfile() {
               <div className="profile-timeline">
                 <ul className="list-unstyled">
                   {profilePosts.length > 0 &&
-                    profilePosts.map((data) => (
-                      <Post
-                        p={data}
-                        key={data.id}
-                      />
-                    ))}
+                    profilePosts.map((data) => <Post p={data} key={data.id} />)}
                 </ul>
               </div>
             </div>
